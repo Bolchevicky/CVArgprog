810a947916c87050e00ad11b916c01f949053409
Bolchevicky <vic914@gmail.com>
2023-02-13 15:52:47 -0300

    corregido el js

--- js/function.js
+++ js/function.js
@@ -1,6 +1,80 @@
-let btn = document.getElementById("btn")
+let hide1 = false;
 
-function ocultar () {
-  document.querySelector("#bxhide").style.display = "none";
+function ocultar1 () {
+  if (hide1 == false) {
+  document.querySelector("#bxhide1").style.display = "none";
+  hide1 = true}
+  else {
+    document.querySelector("#bxhide1").style.display = "inline-table"
+    hide1 = false 
+  }
+  
 }
-btn.addEventListener("click", ocultar)
+
+let hide2 = false;
+
+function ocultar2 () {
+  if (hide2 == false) {
+  document.querySelector("#bxhide2").style.display = "none";
+  hide2 = true}
+  else {
+    document.querySelector("#bxhide2").style.display = "inline-table"
+    hide2 = false 
+  }
+}
+  let hide3 = false;
+
+function ocultar3 () {
+  if (hide3 == false) {
+  document.querySelector("#bxhide3").style.display = "none";
+  hide3 = true}
+  else {
+    document.querySelector("#bxhide3").style.display = "inline-table"
+    hide3 = false 
+  }
+}
+  let hide4 = false;
+
+function ocultar4 () {
+  if (hide4 == false) {
+  document.querySelector("#bxhide4").style.display = "none";
+  hide4 = true}
+  else {
+    document.querySelector("#bxhide4").style.display = "inline-table"
+    hide4 = false 
+  }
+}
+  let hide5 = false;
+
+function ocultar5 () {
+  if (hide5 == false) {
+  document.querySelector("#bxhide5").style.display = "none";
+  hide5 = true}
+  else {
+    document.querySelector("#bxhide5").style.display = "inline-table"
+    hide5 = false 
+  }
+}
+  
+  let hide6 = false;
+
+function ocultar6 () {
+  if (hide6 == false) {
+  document.querySelector("#bxhide6").style.display = "none";
+  hide6 = true}
+  else {
+    document.querySelector("#bxhide6").style.display = "inline-table"
+    hide6 = false 
+  }
+}
+let hide7 = false;
+
+function ocultar7 () {
+  if (hide7 == false) {
+  document.querySelector("#bxhide7").style.display = "none";
+  hide7 = true}
+  else {
+    document.querySelector("#bxhide7").style.display = "inline-table"
+    hide7 = false 
+  }
+}
