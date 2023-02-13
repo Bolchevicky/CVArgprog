974e6a683f97b3a419d64a1fc3c67c49051ac211
Bolchevicky <vic914@gmail.com>
2023-02-13 14:46:51 -0300

    añadido js

--- function.js
+++
@@ -1,9 +0,0 @@
-const div = document.querySelector('div')
-
-document.querySelector('.hide').addEventListener('click',() => {
-  div.classList.add('div_hide')
-})
-
-document.querySelector('.show').addEventListener('click',() => {
-  div.classList.remove('div_hide')
-})
---
+++ js/function.js
@@ -0,0 +1,6 @@
+let btn = document.getElementById("btn")
+
+function ocultar () {
+  document.querySelector("#bxhide").style.display = "none";
+}
+btn.addEventListener("click", ocultar)
